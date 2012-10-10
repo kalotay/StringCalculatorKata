6e279e5cae2074409a071378cf949cc128b95211
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-10 14:27:03 +0100

    initial code commit

---
+++ StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,47 @@
+using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private readonly StringCalculator _stringCalculator = new StringCalculator();
+
+        [Test]
+        public void EmptyStringReturnsZero()
+        {
+            var result = _stringCalculator.Add(String.Empty);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        public void SingleNumberReturnsItself(string number, int expected)
+        {
+            NumberSequenceReturnsTheSum(number, expected);
+        }
+
+       
+        [TestCase("2,3", 5)]
+        [TestCase("11,9", 20)]
+        public void TwoNumbersReturnTheirSum(string numbers, int expected)
+        {
+            NumberSequenceReturnsTheSum(numbers, expected);
+        }
+
+        [TestCase("1,2,3", 6)]
+        public void ThreeNumbersReturnTheirSum(string numbers, int expected)
+        {
+            NumberSequenceReturnsTheSum(numbers, expected);
+        }
+
+        private void NumberSequenceReturnsTheSum(string number, int expected)
+        {
+            var result = _stringCalculator.Add(number);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ StringCalculator/StringClaculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var splitNumbers = numbers.Split(',');
+
+            return splitNumbers.Sum(number => int.Parse(number));
+        }
+    }
+}
