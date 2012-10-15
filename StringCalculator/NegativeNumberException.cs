055ff699f840922563ec9e26cbf612128a27dc43
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-15 17:48:14 +0100

    added custom exception

---
+++ StringCalculator/NegativeNumberException.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace StringCalculator
+{
+    internal class NegativeNumberException : Exception
+    {
+        private string _message = "Received following negative numbers:";
+
+        public void Add(string number)
+        {
+            _message += (" " + number);
+        }
+
+        public override string Message
+        {
+            get { return _message; }
+        }
+
+        public bool IsEmpty()
+        {
+            return Message == "Received following negative numbers:";
+        }
+    }
+}
