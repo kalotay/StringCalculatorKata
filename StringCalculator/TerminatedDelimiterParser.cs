7b33bc120648ee3782db930ce4a0fc29232d5d0d
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-18 17:48:56 +0100

    removed unused code

--- StringCalculator/TerminatedDelimiterParser.cs
+++ StringCalculator/TerminatedDelimiterParser.cs
@@ -5,13 +5,11 @@ namespace StringCalculator
     public class TerminatedDelimiterParser : IDelimiterParser
     {
         public IList<string> Delimiters { get; private set; }
-        public IDelimiterParser Parent { get; private set; }
         public bool HasTerminated { get; private set; }
 
         public TerminatedDelimiterParser(IList<string> delimiters)
         {
             Delimiters = delimiters;
-            Parent = null;
             HasTerminated = true;
         }
 
