e2c27459f75575a79211ad0138e945929f79f970
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-18 14:59:50 +0100

    remove redundant code

--- StringCalculator/DelimiterParser.cs
+++ StringCalculator/DelimiterParser.cs
@@ -29,21 +29,4 @@ namespace StringCalculator
             return this;
         }
     }
-
-    public class TerminatedDelimiterParser : IDelimiterParser
-    {
-        public IList<string> Delimiters { get; private set; }
-        public bool HasTerminated { get; private set; }
-
-        public TerminatedDelimiterParser(IList<string> delimiters)
-        {
-            Delimiters = delimiters;
-            HasTerminated = true;
-        }
-
-        public IDelimiterParser Read(char input)
-        {
-            throw new ParserTerminatedException();
-        }
-    }
 }
