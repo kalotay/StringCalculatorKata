b6ed8efcf4b19046678f6ef9dedc5e8c42381412
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-22 12:07:55 +0100

    initial processor implementation

---
+++ StringCalculator/Processor/MyAdder.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace StringCalculator.Processor
+{
+    public class MyAdder: IProcessor
+    {
+        public int Process(IEnumerable<int> numbers)
+        {
+            return default(int);
+        }
+    }
+}
---
+++ StringCalculator/Processor/ProcessorTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Processor
+{
+    [TestFixture]
+    class ProcessorTests
+    {
+        private MyAdder _processor;
+
+        [SetUp]
+        public void CreateProcessor()
+        {
+            _processor = new MyAdder();
+        }
+
+        [Test]
+        public void EmptySequenceReturnsZero()
+        {
+            var sum = _processor.Process(new int[0]);
+
+            Assert.That(sum, Is.EqualTo(0));
+        }
+    }
+}
