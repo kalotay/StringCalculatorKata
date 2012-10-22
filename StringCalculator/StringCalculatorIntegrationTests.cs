6b69279180becb1feb4e6a98dd09703b5934b7d9
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-22 17:19:49 +0100

    better coverage

--- StringCalculator/StringCalculatorIntegrationTests.cs
+++ StringCalculator/StringCalculatorIntegrationTests.cs
@@ -54,6 +54,7 @@ namespace StringCalculator
         [TestCase("//[*][%]\n1*2%3", 6)]
         [TestCase("//;_\n1_3;3", 7)]
         [TestCase("//[_=]\n1_=2", 3)]
+        [TestCase("//[#][@]\n1#2@3", 6)]
         public void DelimiterCanBeSpecified(string delimitersAndNumbers, int expected)
         {
             NumberSequenceReturnsTheSum(delimitersAndNumbers, expected);
