c0718612bc441674b2dbaa7503ed304af0cfcaea
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-22 10:07:46 +0100

    first parser tests

---
+++ StringCalculator/Parser/ParserTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace StringCalculator.Parser
+{
+    [TestFixture]
+    class ParserTests
+    {
+        private IParser _parser;
+
+        [SetUp]
+        public void CreateParser()
+        {
+            _parser = new MessageParser();
+        }
+
+        [Test]
+        public void emptyStringReturnsEmpty()
+        {
+            var numbers = _parser.Parse(string.Empty);
+
+            Assert.That(numbers, Is.Empty);
+        }
+    }
+
+    internal class MessageParser : IParser
+    {
+        public IEnumerable<int> Parse(string message)
+        {
+            return (new int[0]).AsEnumerable();
+        }
+    }
+}
