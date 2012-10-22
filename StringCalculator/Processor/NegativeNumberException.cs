6c864146a2b36142b77d7acf80da7fcf6d52750e
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-22 16:59:15 +0100

    negative number exception should contain the negative numbers initiated with

--- StringCalculator/Processor/NegativeNumberException.cs
+++ StringCalculator/Processor/NegativeNumberException.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StringCalculator.Processor
 {
     public class NegativeNumberException : Exception
     {
+        public readonly int[] NegativeNumbers;
+
         private readonly string _message;
         private const string MessagePrefix = "Received following negative numbers: ";
 
         public NegativeNumberException(IEnumerable<int> negativeNumbers)
         {
-            _message  = MessagePrefix + string.Join(", ", negativeNumbers);
+            NegativeNumbers = negativeNumbers.ToArray();
+            _message = MessagePrefix + string.Join(", ", NegativeNumbers);
         }
 
         public override string Message
