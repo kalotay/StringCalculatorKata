de4a4b5aa1383cb54f6d4598fd9b5395383a7cc6
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-22 10:41:39 +0100

    default delimiters as field

--- StringCalculator/Parser/MessageParser.cs
+++ StringCalculator/Parser/MessageParser.cs
@@ -5,11 +5,14 @@ namespace StringCalculator.Parser
 {
     public class MessageParser : IParser
     {
+        private char[] _defaultDelimiters = new [] {',', '\n'};
+
         public IEnumerable<int> Parse(string message)
         {
+            _defaultDelimiters = new[] {',', '\n'};
             var numbers = string.IsNullOrEmpty(message)
                               ? new int[0].AsEnumerable()
-                              : message.Split(new char[] {',', '\n'}).Select(int.Parse);
+                              : message.Split(_defaultDelimiters).Select(int.Parse);
 
             return numbers;
         }
