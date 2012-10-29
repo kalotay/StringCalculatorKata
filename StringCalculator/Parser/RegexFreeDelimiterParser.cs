5c0ca9356b9f0b1b9004632095d04bcdecbbdcef
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-29 12:30:07 +0000

    created regexfree parser

---
+++ StringCalculator/Parser/RegexFreeDelimiterParser.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using StringCalculator.Lexer;
+
+namespace StringCalculator.Parser
+{
+    class RegexFreeDelimiterParser: IParser
+    {
+        private readonly Regex _defaultSplitter;
+
+        public RegexFreeDelimiterParser(IEnumerable<string> defaultDelimiters)
+        {
+            _defaultSplitter = defaultDelimiters.GenerateSplitter();
+        }
+
+        public IEnumerable<int> Parse(string message)
+        {
+            var lexer = new StringCalculatorLexer(message);
+            var delimiters = new HashSet<string>();
+            string numbersString = null;
+
+            foreach (var token in lexer.Read())
+            {
+                if (token.Type == StringCalculatorToken.Types.Delimiter)
+                {
+                    delimiters.Add(token.Content);
+                }
+
+                if (token.Type == StringCalculatorToken.Types.Numbers)
+                {
+                    numbersString = token.Content;
+                }
+            }
+
+            if (string.IsNullOrEmpty(numbersString))
+            {
+                return Enumerable.Empty<int>();
+            }
+
+            var numberSplitter = delimiters.Any() ?
+                delimiters.GenerateSplitter() :
+                _defaultSplitter;
+
+            return numberSplitter
+                .Split(numbersString)
+                .Select(int.Parse);
+        }
+    }
+}
---
+++ StringCalculator/Parser/RegexFreeParserTests.cs
@@ -0,0 +1,121 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace StringCalculator.Parser
+{
+    [TestFixture]
+    class RegexFreeParserTests
+    {
+        private IParser _parser;
+
+        [SetUp]
+        public void CreateParser()
+        {
+            _parser = new RegexFreeDelimiterParser(new[] {",", "\n"});
+        }
+
+        [Test]
+        public void EmptyStringReturnsEmpty()
+        {
+            var numbers = _parser.Parse(string.Empty);
+
+            Assert.That(numbers, Is.Empty);
+        }
+
+        [Test]
+        public void SingleNumberStringReturnsSingleItemSequenceWithNumber()
+        {
+            var number = _parser.Parse("16");
+
+            Assert.That(number.Count(), Is.EqualTo(1));
+            Assert.That(number, Contains.Item(16));
+        }
+
+        [Test]
+        public void NumbersCanBeCommaSeparated()
+        {
+            var numbers = _parser.Parse("10,9,8");
+
+            Assert.That(numbers.Count(), Is.EqualTo(3));
+            Assert.That(numbers, Contains.Item(10));
+            Assert.That(numbers, Contains.Item(9));
+            Assert.That(numbers, Contains.Item(8));
+        }
+
+        [Test]
+        public void NumbersCanBeNewlineSeparated()
+        {
+            var numbers = _parser.Parse("7\n6\n5");
+
+            Assert.That(numbers.Count(), Is.EqualTo(3));
+            Assert.That(numbers, Contains.Item(7));
+            Assert.That(numbers, Contains.Item(6));
+            Assert.That(numbers, Contains.Item(5));
+        }
+
+        [Test]
+        public void NumbersCanBeCommaAndNewlineSeparated()
+        {
+            var numbers = _parser.Parse("4,3\n2");
+
+            Assert.That(numbers.Count(), Is.EqualTo(3));
+            Assert.That(numbers, Contains.Item(4));
+            Assert.That(numbers, Contains.Item(3));
+            Assert.That(numbers, Contains.Item(2));
+        }
+
+        [Test]
+        public void DelimitersCanBeSpecified()
+        {
+            var numbers = _parser.Parse("//;_\n1;0_-1");
+
+            Assert.That(numbers.Count(), Is.EqualTo(3));
+            Assert.That(numbers, Contains.Item(1));
+            Assert.That(numbers, Contains.Item(0));
+            Assert.That(numbers, Contains.Item(-1));
+        }
+
+        [Test]
+        public void MultiCharDelimitersCanBeSpecified()
+        {
+            var numbers = _parser.Parse("//[_:]\n1000_:100");
+
+            Assert.That(numbers.Count(), Is.EqualTo(2));
+            Assert.That(numbers, Contains.Item(1000));
+            Assert.That(numbers, Contains.Item(100));
+        }
+
+        [Test]
+        public void MultiCharDelimitersCanContainNewlines()
+        {
+            var numbers = _parser.Parse("//[;\n]\n1;\n2");
+
+            Assert.That(numbers.Count(), Is.EqualTo(2));
+            Assert.That(numbers, Contains.Item(1));
+            Assert.That(numbers, Contains.Item(2));
+        }
+
+        [Test]
+        public void MultiCharDelimitersCanContainOpeningSquareBrace()
+        {
+            var numbers = _parser.Parse("//[;[]\n1;[2");
+
+            Assert.That(numbers.Count(), Is.EqualTo(2));
+            Assert.That(numbers, Contains.Item(1));
+            Assert.That(numbers, Contains.Item(2));
+        }
+
+        [Test]
+        public void MultiCharacterDelimiterSubsetOfAnotherCanBeSpecified()
+        {
+            var numbers = _parser.Parse("//[aa][aaa]\n1aaa2aa3");
+
+            Assert.That(numbers.Count(), Is.EqualTo(3));
+            Assert.That(numbers, Contains.Item(1));
+            Assert.That(numbers, Contains.Item(2));
+            Assert.That(numbers, Contains.Item(3));
+        }
+
+
+    }
+}
