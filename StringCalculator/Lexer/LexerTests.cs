6b1546709772ce3e8c4a22e0dcaf920a8e8fff8c
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-10-29 10:25:33 +0000

    single delimiters can be lexed

--- StringCalculator/Lexer/LexerTests.cs
+++ StringCalculator/Lexer/LexerTests.cs
@@ -26,5 +26,29 @@ namespace StringCalculator.Lexer
             Assert.That(numbers.Type, Is.EqualTo(StringCalculatorToken.Types.Numbers));
             Assert.That(numbers.Content, Is.EqualTo("1,2,3"));
         }
+
+        [Test]
+        public void SingleCharacterDelimitersCanBeSpecified()
+        {
+            var lexer = new StringCalculatorLexer("//;_\n1;2_3");
+            var tokens = lexer.Read().ToArray();
+            var semiColon = tokens[1];
+            var underScore = tokens[2];
+            var numbers = tokens[4];
+
+            Assert.That(tokens.Count(), Is.EqualTo(5));
+
+            Assert.That(tokens[0].Type, Is.EqualTo(StringCalculatorToken.Types.DelimitersStart));
+
+            Assert.That(semiColon.Type, Is.EqualTo(StringCalculatorToken.Types.Delimiter));
+            Assert.That(semiColon.Content, Is.EqualTo(";"));
+            Assert.That(underScore.Type, Is.EqualTo(StringCalculatorToken.Types.Delimiter));
+            Assert.That(underScore.Content, Is.EqualTo("_"));
+
+            Assert.That(tokens[3].Type, Is.EqualTo(StringCalculatorToken.Types.DelimitersEnd));
+
+            Assert.That(numbers.Type, Is.EqualTo(StringCalculatorToken.Types.Numbers));
+            Assert.That(numbers.Content, Is.EqualTo("1;2_3"));
+        }
     }
 }
--- StringCalculator/Lexer/StringCalculatorLexer.cs
+++ StringCalculator/Lexer/StringCalculatorLexer.cs
@@ -4,11 +4,13 @@ namespace StringCalculator.Lexer
 {
     public class StringCalculatorLexer
     {
-        private string _message;
+        private readonly string _message;
+        private int _position;
 
         public StringCalculatorLexer(string message)
         {
              _message = message;
+            Reset();
         }
 
         public IEnumerable<StringCalculatorToken> Read()
@@ -18,7 +20,38 @@ namespace StringCalculator.Lexer
                 yield break;
             }
 
-            yield return new StringCalculatorToken {Content = _message, Type = StringCalculatorToken.Types.Numbers};
+            var hasDelimiterSpec = false;
+
+            if (_message.StartsWith("//"))
+            {
+                hasDelimiterSpec = true;
+                _position += 2;
+                yield return new StringCalculatorToken {Type = StringCalculatorToken.Types.DelimitersStart};
+            }
+
+            while (hasDelimiterSpec)
+            {
+                var currentChar = _message[_position];
+                _position += 1;
+
+                if (currentChar == '\n')
+                {
+                    hasDelimiterSpec = false;
+                    yield return new StringCalculatorToken {Type = StringCalculatorToken.Types.DelimitersEnd};
+                }
+                else
+                {
+                    yield return new StringCalculatorToken {Type = StringCalculatorToken.Types.Delimiter, Content = new string(currentChar, 1)};
+                }
+            }
+
+            yield return new StringCalculatorToken {Content = _message.Substring(_position), Type = StringCalculatorToken.Types.Numbers};
+        }        
+        
+        private void Reset()
+        {
+            _position = 0;
         }
+
     }
 }
